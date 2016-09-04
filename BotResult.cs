2cd3ee03aaf7ad3dde58a02ea339a777fce570de
taha zayed <tahazayed@gmail.com>
2016-09-04 22:45:50 +0200

    Initial commit

---
+++ BotResult.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MrSamwiseBot
+{
+
+    public class Action
+    {
+        public string title { get; set; }
+        public string message { get; set; }
+    }
+
+    public class Attachment
+    {
+        public List<Action> actions { get; set; }
+    }
+
+    public class From
+    {
+        public string name { get; set; }
+        public string channelId { get; set; }
+        public string address { get; set; }
+        public bool isBot { get; set; }
+    }
+
+    public class To
+    {
+        public string name { get; set; }
+        public string channelId { get; set; }
+        public string address { get; set; }
+        public bool isBot { get; set; }
+    }
+
+    public class Participant
+    {
+        public string name { get; set; }
+        public string channelId { get; set; }
+        public string address { get; set; }
+    }
+
+    public class BotUserData
+    {
+    }
+
+    public class BotConversationData
+    {
+    }
+
+    public class BotPerUserInConversationData
+    {
+        public string DialogState { get; set; }
+    }
+
+    public class BotResult
+    {
+        public string conversationId { get; set; }
+        public string language { get; set; }
+        public string text { get; set; }
+        public List<Attachment> attachments { get; set; }
+        public From from { get; set; }
+        public To to { get; set; }
+        public string replyToMessageId { get; set; }
+        public List<Participant> participants { get; set; }
+        public int totalParticipants { get; set; }
+        public string channelMessageId { get; set; }
+        public string channelConversationId { get; set; }
+        public BotUserData botUserData { get; set; }
+        public BotConversationData botConversationData { get; set; }
+        public BotPerUserInConversationData botPerUserInConversationData { get; set; }
+    }
+}
---
+++ MainForm.cs
@@ -0,0 +1,132 @@
+﻿using Microsoft.Bot.Connector.Emulator;
+using Microsoft.Lync.Model;
+using Microsoft.Lync.Model.Conversation;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+
+namespace MrSamwiseBot
+{
+    public partial class MainForm : Form
+    {
+        private LyncClient _lyncClient;
+        private ConversationManager _conversationManager;
+        private readonly ConnectorEmulator oConnectorEmulator = new ConnectorEmulator();
+
+
+
+        string response=string.Empty;
+
+        public MainForm()
+        {
+            InitializeComponent();
+
+            _lyncClient = LyncClient.GetClient();
+            _conversationManager = _lyncClient.ConversationManager;
+            _conversationManager.ConversationAdded += ConversationAdded;
+            try
+            {
+                oConnectorEmulator.Port = Properties.Settings.Default.ConnectorEmulatorPort;
+                oConnectorEmulator.StartServer();
+            }
+            catch
+            {
+                MessageBox.Show(string.Format("I can not start as Port:{0} is used by another application", Properties.Settings.Default.ConnectorEmulatorPort));
+            }
+  
+            
+        }
+
+        private void ConversationAdded(object sender, ConversationManagerEventArgs e)
+        {
+            var conversation = e.Conversation;
+            conversation.ParticipantAdded += ParticipantAdded;
+        }
+
+        private void ParticipantAdded(object sender, ParticipantCollectionChangedEventArgs e)
+        {
+            var participant = e.Participant;
+            if (participant.IsSelf)
+            {
+                return;
+            }
+
+            var instantMessageModality =
+                e.Participant.Modalities[ModalityTypes.InstantMessage] as InstantMessageModality;
+            instantMessageModality.InstantMessageReceived += InstantMessageReceived;
+        }
+
+        private async void InstantMessageReceived(object sender, MessageSentEventArgs e)
+        {
+            var text = e.Text.Replace(Environment.NewLine, string.Empty);
+            string myRemoteParticipantUri = (sender as InstantMessageModality).Endpoint.Uri.Replace("sip:", string.Empty);
+            LogMessage(myRemoteParticipantUri, text);
+            try
+            {
+                ConversationModel oConversationModel = new ConversationModel(oConnectorEmulator,
+                    Properties.Settings.Default.BotURL,
+                    Properties.Settings.Default.AppId,
+                    Properties.Settings.Default.AppSecret,
+                    myRemoteParticipantUri,
+                    myRemoteParticipantUri,
+                    "en-US");
+
+                var oMessage = oConversationModel.CreateMessage(text, "Message");
+                var oResponse = await oConversationModel.SendMessageAsync(oMessage);
+                BotResult oBotResult = new BotResult();
+                oBotResult = JsonConvert.DeserializeObject<BotResult>(oResponse.Content);
+
+                string strResult = oBotResult.text;
+                foreach (var att in oBotResult.attachments)
+                {
+                    strResult += Environment.NewLine;
+                    foreach (var act in att.actions)
+                    {
+                        strResult += "   " + act.title + Environment.NewLine;
+                    }
+                }
+            (sender as InstantMessageModality).BeginSendMessage(strResult, null, null);
+            }
+            catch(Exception ex)
+            {
+                LogMessage(myRemoteParticipantUri, ex.Message);
+            }
+        }
+
+       
+        void StartConversation(string myRemoteParticipantUri, string MSG)
+        {
+           
+            foreach (var con in _conversationManager.Conversations)
+            {
+                if (con.Participants.Where(p => p.Contact.Uri == "sip:" + myRemoteParticipantUri).Count() > 0)
+                {
+                    if (con.Participants.Count == 2)
+                    {
+                        con.End();
+                        break;
+                    }
+                }
+            }
+
+            Conversation _Conversation = _conversationManager.AddConversation();
+            _Conversation.AddParticipant(_lyncClient.ContactManager.GetContactByUri(myRemoteParticipantUri));
+
+
+            Dictionary<InstantMessageContentType, String> messages = new Dictionary<InstantMessageContentType, String>();
+            messages.Add(InstantMessageContentType.PlainText, MSG);
+            InstantMessageModality m = (InstantMessageModality)_Conversation.Modalities[ModalityTypes.InstantMessage];
+            m.BeginSendMessage(messages, null, messages);
+            LogMessage(myRemoteParticipantUri, MSG);
+        }
+
+        private static void LogMessage(string myRemoteParticipantUri, string MSG)
+        {
+            
+        }
+
+    }
+}
